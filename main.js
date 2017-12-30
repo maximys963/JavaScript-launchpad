8fc473592531e672ebd7aea6850531c8b7f17a39
Maxim <maxrap7878@mail.ru>
2017-12-30 17:54:03 +0200

    final version

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-window.addEventListener('keydown', function(e){
+function removeTransition(e){
+    // debugger;
+    if(e.propertyName !== 'transform') return;
+    e.target.classList.remove('playing');
+}
+
+function playSound (e){
+    // debugger;
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    console.log(audio);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    // console.log(audio);
     if(!audio) return ;
+    key.classList.add('playing');
+    audio.currentTime = 0;
     audio.play();
-})
+
+}
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
+console.log("hello world");
